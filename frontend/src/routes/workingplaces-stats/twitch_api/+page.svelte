26d14e7a35ff1f22f9ca5902cf4ab640fe7de7a0
PABLO VERA LOPEZ <pabverlop1@alum.us.es>
2023-10-27 14:51:53 +0200

    Twitch

--- frontend/src/routes/workingplaces-stats/twitch_api/+page.svelte
+++ frontend/src/routes/workingplaces-stats/twitch_api/+page.svelte
@@ -15,7 +15,7 @@
     let redirect_uri = "https://sos2223-dic-pvl.ew.r.appspot.com/workingplaces-stats/twitch_api"
     let code;
 
-    let userId;
+    let name;
 
 
     onMount(async () => {
@@ -28,7 +28,7 @@
         code = urlParams.get('code');
         client_id = localStorage.getItem("client_id");
         client_secret = localStorage.getItem("client_secret");
-        userId = localStorage.getItem("userId");
+        name = localStorage.getItem("name");
         if ( code ){
             asignacion_code();
         }
@@ -42,7 +42,7 @@
         async function getAuth(){
         localStorage.setItem("client_id", client_id);
         localStorage.setItem("client_secret", client_secret);
-        localStorage.setItem("userId", userId);
+        localStorage.setItem("name", name);
         let url = auth+"?client_id=" + client_id+ "&response_type=code"+ "&redirect_uri=" + redirect_uri;
         window.location.href = url;
     }
@@ -81,7 +81,7 @@
     }
     async function getCanciones() {
       const authToken = acces_token;
-      const responseUser = await fetch(`https://api.twitch.tv/helix/games?name=Valorant`, {
+      const responseUser = await fetch(`https://api.twitch.tv/helix/games?name={name}`, {
         headers: {
           'Client-ID': client_id,
           'Authorization': `Bearer ${authToken}`
@@ -102,20 +102,17 @@
                     <Input type="text" name="text" id="clientId" bind:value={client_id}/>
                     <Label for="clientSecret">Client Secret:</Label>
                     <Input type="text" name="text" id="clientSecret" bind:value={client_secret}/>
-                    <Label for="userId">User ID:</Label>
-                    <Input type="text" name="text" id="userId" bind:value={userId}/>
+                    <Label for="name">Nombre del Juego:</Label>
+                    <Input type="text" name="text" id="name" bind:value={name}/>
                     <Button color="primary" on:click={getAuth}>Pedir autorización</Button>
-                    <Button color="primary" on:click={getCanciones}>Actualizar Videos</Button>
+                    <Button color="primary" on:click={getCanciones}>Actualizar Juego</Button>
                 
             </Form>    
             <div class="ImageContainer">
-              {#each datam as dato}
-                <div class="ImageBox">
-                  <img class="Image" src={dato.album.image_url} alt={dato.album.name}>
-                  <a class="Title" href="">{dato.album.name}</a>
-                </div>
-              {/each}
-            </div>     
+  <div class="ImageBox">
+    <img class="Image" src={datam[0].box_art_url.replace('{width}', '100').replace('{height}', '100')} alt={datam[0].name}>
+    <a class="Title" href="">{datam[0].name}</a>
+  </div>  
     </div>
 </main>
 <style>
